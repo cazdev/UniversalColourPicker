46fb6b055f8f9e350bc4993da617bfb6e66f7c30
Chaz L <chaz6cl@live.com>
2019-04-17 19:16:20 +1000

    Colour Picker

---
+++ ColourPicker/ColourPicker.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ColourPicker
+{
+    partial class ColourPicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ColourPicker));
+            this.BtnCopyHex = new System.Windows.Forms.Button();
+            this.LblHex = new System.Windows.Forms.Label();
+            this.BtnCopyRGB = new System.Windows.Forms.Button();
+            this.LblRGB = new System.Windows.Forms.Label();
+            this.MoveMouse = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+
+            ColourPicker.Start();
+            ColourPicker.MouseAction += new System.EventHandler(Event);
+
+            // 
+            // BtnCopyHex
+            // 
+            this.BtnCopyHex.BackColor = System.Drawing.Color.White;
+            this.BtnCopyHex.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BtnCopyHex.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BtnCopyHex.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCopyHex.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCopyHex.Location = new System.Drawing.Point(103, 190);
+            this.BtnCopyHex.Name = "BtnCopyHex";
+            this.BtnCopyHex.Size = new System.Drawing.Size(75, 31);
+            this.BtnCopyHex.TabIndex = 7;
+            this.BtnCopyHex.Text = "Copy";
+            this.BtnCopyHex.UseVisualStyleBackColor = false;
+            this.BtnCopyHex.Click += new System.EventHandler(this.BtnCopyHex_Click);
+            // 
+            // LblHex
+            // 
+            this.LblHex.BackColor = System.Drawing.Color.White;
+            this.LblHex.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblHex.Location = new System.Drawing.Point(0, 142);
+            this.LblHex.Name = "LblHex";
+            this.LblHex.Size = new System.Drawing.Size(284, 45);
+            this.LblHex.TabIndex = 6;
+            this.LblHex.Text = "FFFFFF";
+            this.LblHex.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnCopyRGB
+            // 
+            this.BtnCopyRGB.BackColor = System.Drawing.Color.White;
+            this.BtnCopyRGB.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BtnCopyRGB.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BtnCopyRGB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCopyRGB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCopyRGB.Location = new System.Drawing.Point(103, 82);
+            this.BtnCopyRGB.Name = "BtnCopyRGB";
+            this.BtnCopyRGB.Size = new System.Drawing.Size(75, 31);
+            this.BtnCopyRGB.TabIndex = 5;
+            this.BtnCopyRGB.Text = "Copy";
+            this.BtnCopyRGB.UseVisualStyleBackColor = false;
+            this.BtnCopyRGB.Click += new System.EventHandler(this.BtnCopyRGB_Click);
+            // 
+            // LblRGB
+            // 
+            this.LblRGB.BackColor = System.Drawing.Color.White;
+            this.LblRGB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRGB.Location = new System.Drawing.Point(0, 40);
+            this.LblRGB.Name = "LblRGB";
+            this.LblRGB.Size = new System.Drawing.Size(284, 39);
+            this.LblRGB.TabIndex = 4;
+            this.LblRGB.Text = "255, 255, 255";
+            this.LblRGB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MoveMouse
+            // 
+            this.MoveMouse.Enabled = true;
+            this.MoveMouse.Interval = 1;
+            // 
+            // ColourPicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.BtnCopyHex);
+            this.Controls.Add(this.LblHex);
+            this.Controls.Add(this.BtnCopyRGB);
+            this.Controls.Add(this.LblRGB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ColourPicker";
+            this.Text = "ColourPicker";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCopyHex;
+        private System.Windows.Forms.Label LblHex;
+        private System.Windows.Forms.Button BtnCopyRGB;
+        private System.Windows.Forms.Label LblRGB;
+        private System.Windows.Forms.Timer MoveMouse;
+    }
+}
+
---
+++ ColourPicker/ColourPicker.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace ColourPicker
+{
+    public partial class ColourPicker : Form
+    {
+
+        public static string rgb = "255, 255, 255";
+        public static string hex = "FFFFFF";
+
+        public ColourPicker()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+
+        private void Event(object sender, EventArgs e)
+        {
+            Point cursor = new Point();
+            GetCursorPos(ref cursor);
+
+            if (!new RectangleF(this.Location.X, this.Location.Y, this.Width, this.Height).Contains(cursor))
+            {
+                var c = GetColorAt(cursor);
+                this.BackColor = c;
+
+                rgb = $"{c.R}, {c.G}, {c.B}";
+
+                hex = c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+
+                LblRGB.Text = rgb;
+
+                LblHex.Text = hex;
+            }
+        }
+
+        public static event EventHandler MouseAction = delegate { };
+
+        public static void Start()
+        {
+            _hookID = SetHook(_proc);
+
+
+        }
+        public static void stop()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static LowLevelMouseProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                IntPtr hook = SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle("user32"), 0);
+                if (hook == IntPtr.Zero) throw new System.ComponentModel.Win32Exception();
+                return hook;
+            }
+        }
+
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+          int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                MouseAction(null, new EventArgs());
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_MOUSE_LL = 14;
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+          LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+          IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private void BtnCopyRGB_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(rgb);
+        }
+
+        private void BtnCopyHex_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(hex);
+        }
+    }
+    
+}
